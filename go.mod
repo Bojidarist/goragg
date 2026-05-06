4bcf96f182dd062d98fb4f8a93b12c0242c36e45
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2026-05-06 03:10:58 +0300

    Basic http server with chi

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/Bojidarist/goragg
 
 go 1.26.1
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi/v5 v5.2.5
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/go-chi/cors v1.2.2
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -18,5 +21,38 @@ func main() {
 		log.Fatal("PORT environment variable must be defined!")
 	}
 
-	log.Println("Port:", port)
+	router := chi.NewRouter()
+
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/ready", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	address := ":" + port
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    address,
+	}
+
+	log.Println("Server starting on:", address)
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
